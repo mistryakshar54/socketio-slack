8c89e9627f32c695fa0fbd6b3c6759000d26aa39
Akshar Mistry <mistry.aks@gmail.com>
2020-05-02 17:34:20 +0530

    add eslint

--- slack-server/src/index.ts
+++ slack-server/src/index.ts
@@ -1,35 +1,36 @@
-import express, { Request , Response , NextFunction } from 'express';
+/* eslint-disable no-console */
+import express, { Request , Response } from 'express';
 import socketIo from 'socket.io';
 import NameSpace from './models/namespace';
-const NameSpaces : NameSpace[] = [
+const NameSpaces: NameSpace[] = [
   new NameSpace('wiki', '1234', ['artciles', 'news'], ''),
   new NameSpace('gameing', '1234', ['pubg','cod','counter-strike'], ''),
 ];
 
 const app = express();
-app.use("/slack", (req : Request, res: Response) => {
-  res.sendFile(__dirname + "/public/chat.html");
+app.use('/slack', (req: Request, res: Response) => {
+  res.sendFile(__dirname + '/public/chat.html');
 });
 
-app.use("/slack2", (req : Request, res: Response) => {
-  res.sendFile(__dirname + "/public/chat2.html");
+app.use('/slack2', (req: Request, res: Response) => {
+  res.sendFile(__dirname + '/public/chat2.html');
 });
 
-app.use('/', ( req: Request , res : Response ) => {
-  res.send({message : "Welcome!"});
-})
+app.use('/', ( req: Request , res: Response ) => {
+  res.send({message : 'Welcome!'});
+});
 
 const appInstance = app.listen(4000);
 appInstance.on('listening' , () => {
-  console.log("Server listening on port: 4000");
-})
+  console.log('Server listening on port: 4000');
+});
 
 const getNamespaceList = () => {
-  return NameSpaces.map( (namespace : NameSpace) => {
+  return NameSpaces.map( (namespace: NameSpace) => {
     return {
       name : namespace.name,
       icon : namespace.icon
-    }
+    };
   });
 };
 
@@ -39,25 +40,25 @@ io.on('connection', ( socket ) => {
   const namespaceList = getNamespaceList();
   socket.emit('mainSocketMsg', {
     message: {
-      type: "namespaceList",
+      type: 'namespaceList',
       data: namespaceList,
     },
   });
-})
+});
 
 NameSpaces.forEach((ns) => {
   io.of(ns.name).on('connection' , ( socket ) => {
     console.log(`${socket.id} has joined namespace ${ns.name}`);
     socket.emit(`${ns.name}NSMsg`, { type:'connectHandshake', data:{rooms : ns.rooms}, message: `Connected to Namespace : ${ns.name}` });
     socket.on(`${ns.name}NSClientMsg` , clientMsg => {
-      const { type , message, room } = clientMsg;
+      const { type , room } = clientMsg;
       if(type === 'joinRoom'){
         console.log(`Request from ${socket.id} to join room: ${room}`);
-        socket.join(`${room}`, (err) => {
+        socket.join(`${room}`, () => {
           io.of(`${ns.name}`)
             .in(`${room}`)
             .emit(`${ns.name}NSRoomMsg`, {
-              type: "connectHandshake",
+              type: 'connectHandshake',
               message: `Room: ${room} connected!!`,
             });
         });
