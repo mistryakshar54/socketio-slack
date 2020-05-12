eb434245e98fe00cb0883a9ff4c1d7a52036acfc
Akshar Mistry <mistry.aks@gmail.com>
2020-05-13 02:48:32 +0530

    Change socket url based on host

--- slack-client/src/constants.js
+++ slack-client/src/constants.js
@@ -1,1 +1,17 @@
-export const SOCKET_URL = "http://localhost:4000/";
+import { faGamepad, faCode } from "@fortawesome/free-solid-svg-icons";
+export const getSocketUrl = () => {
+    if(window.location.href.includes('localhost')){
+        return "http://localhost:4000/"; //When in local PORT is 4000 for Node Server
+    }
+    else{
+        return '/';
+    }
+}
+
+export const getIconNameFromString = (iconName) => {
+    switch (iconName) {
+      case "faCode": return faCode;
+      case "faGamepad": return faGamepad;
+      default: return faGamepad;
+    }
+}
